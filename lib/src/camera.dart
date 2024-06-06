a7174fb9e97c540b21d34f016742765d5dad9296
limit <limitliu@qq.com>
2024-06-07 03:05:22 +0800

    feat: limited to Android platform only

--- lib/src/camera.dart
+++ lib/src/camera.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -14,14 +16,31 @@ class _CameraState extends State<Camera> {
   CameraController? _controller;
   bool _detecting = false;
 
+  final _text = ValueNotifier("");
+
+  String get text => _text.value;
+
+  set text(String newValue) {
+    if (newValue == text) {
+      return;
+    }
+    _text.value = newValue;
+  }
+
   @override
   void initState() {
     super.initState();
+    if (!Platform.isAndroid) {
+      return;
+    }
     availableCameras().then((cameras) async {
       final description = cameras
           .firstWhere((e) => e.lensDirection == CameraLensDirection.back);
-      _controller = CameraController(description, ResolutionPreset.max,
-          enableAudio: false);
+      _controller = CameraController(
+        description,
+        ResolutionPreset.max,
+        enableAudio: false,
+      );
       await _controller?.initialize();
       if (!mounted) {
         return;
@@ -37,19 +56,13 @@ class _CameraState extends State<Camera> {
             width: image.width,
             height: image.height,
           );
-          if (kDebugMode) {
-            print("decode: $result");
-          }
+          text = result;
+          _controller?.stopImageStream();
+          _controller?.pausePreview();
         } catch (e) {
           _detecting = false;
-          if (e is CameraException) {
-            if (kDebugMode) {
-              print(e.code);
-            }
-          } else {
-            if (kDebugMode) {
-              print(e);
-            }
+          if (kDebugMode) {
+            print(e);
           }
         }
       });
@@ -63,12 +76,27 @@ class _CameraState extends State<Camera> {
     return Scaffold(
       appBar: AppBar(title: const Text('Camera')),
       body: _controller != null && _controller!.value.isInitialized
-          ? Center(
-              child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 32),
-                  width: double.infinity,
-                  height: width - 64,
-                  child: CameraPreview(_controller!)),
+          ? Column(
+              children: [
+                ValueListenableBuilder(
+                  valueListenable: _text,
+                  builder: (context, v, _) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    child: Platform.isAndroid
+                        ? Text(v)
+                        : const Text(
+                            "The current demo only supports the Android platform."),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Platform.isAndroid
+                    ? Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 32),
+                        width: double.infinity,
+                        height: width - 64,
+                        child: CameraPreview(_controller!))
+                    : const SizedBox.shrink(),
+              ],
             )
           : const Center(child: CircularProgressIndicator()),
     );
@@ -84,7 +112,7 @@ class _CameraState extends State<Camera> {
 
 extension Bytes on CameraImage {
   List<int> get bytes {
-    final buffer = List.filled(width * height * 3, 0);
+    final buffer = List.filled(width * height * 4, 0);
     for (int x = 0; x < width; ++x) {
       for (int y = 0; y < height; ++y) {
         final pixelColor = planes[0].bytes[y * width + x];
--- rust/src/api/bar_decoder.rs
+++ rust/src/api/bar_decoder.rs
@@ -5,7 +5,7 @@ use super::err;
 pub fn decode(bytes: Vec<u8>, width: u32, height: u32) -> err::Result<String> {
   let decoder = bardecoder::default_decoder();
   let img = ImageBuffer::from_vec(width, height, bytes).unwrap();
-  let img = DynamicImage::ImageRgb8(img);
+  let img = DynamicImage::ImageRgba8(img);
   let results = decoder.decode(&img);
   let result = &results[0];
   match result {
@@ -13,4 +13,3 @@ pub fn decode(bytes: Vec<u8>, width: u32, height: u32) -> err::Result<String> {
     Err(e) => Err(err::FFIError::QrCode(e.to_string())),
   }
 }
-
